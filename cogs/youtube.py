ae3a0ae2de06396aab001ac2bc8e2c49bf1b3cd0
achiyang <sinhouse2@gmail.com>
2023-10-16 04:49:07 +0900

    ^Q^~

--- cogs/youtube.py
+++ cogs/youtube.py
@@ -8,6 +8,7 @@ from discord.ext.commands import Context
 from discord.interactions import Interaction
 from discord.partial_emoji import PartialEmoji
 from googleapiclient.discovery import build
+from collections import Counter
 import asyncio
 import aiohttp
 import feedparser
@@ -30,18 +31,21 @@ class Youtube(commands.Cog, name="youtube"):
         notification_channel = await self.bot.fetch_channel(notification_channel_id)
         await notification_channel.send(video_url)
 
-    async def fetch_youtube_video(self, channel_id, count: int = 0) -> list | None:
+    async def fetch_youtube_video(self, channel_id, count: int = 0) -> dict | None:
         async with aiohttp.ClientSession() as session:
             async with session.get(f'https://www.youtube.com/feeds/videos.xml?channel_id={channel_id}', allow_redirects=True) as response:
                 if response.status == 200:
-                    video_ids = []
+                    video_ids = {}
                     data = await response.text()
                     feed = await asyncio.to_thread(feedparser.parse, data)
                     for i in range(len(feed["entries"])):
                         video_id = feed["entries"][i]["yt_videoid"]
-                        if video_id in youtube_channels[channel_id]["video_id"]:
-                            break
-                        video_ids.append(video_id)
+                        if feed["entries"][i]["updated_parsed"]:
+                            updated = feed["entries"][i]["updated_parsed"]
+                            video_ids[video_id] = updated
+                        else:
+                            published = feed["entries"][i]["published_parsed"]
+                            video_ids[video_id] = published
                     return video_ids
                 elif count < 2:
                     return await self.fetch_youtube_video(channel_id, count + 1)
@@ -52,8 +56,13 @@ class Youtube(commands.Cog, name="youtube"):
         video_ids = await self.fetch_youtube_video(channel_id)
         if video_ids != None:
             for video_id in video_ids:
-                await self.send_new_video_link(video_id)
-            youtube_channels[channel_id]["video_id"] = video_ids + youtube_channels[channel_id]["video_id"]
+                if video_id in youtube_channels[channel_id]["video_id"]:
+                    if youtube_channels[channel_id]["video_id"][video_id] != video_ids[video_id]:
+                        youtube_channels[channel_id]["video_id"][video_id] = video_ids[video_id]
+                        await self.send_new_video_link(video_id)
+                else:
+                    youtube_channels[channel_id]["video_id"][video_id] = video_ids[video_id]
+                    await self.send_new_video_link(video_id)
             with open(f"{os.path.realpath(os.path.dirname(os.path.dirname(__file__)))}/data/youtube_channels.json", "w") as f : 
                 json.dump(youtube_channels, f, indent=4)
 
