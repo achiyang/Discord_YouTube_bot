15b14bd77af97f7ad9726107597070415684b5bc
achiyang <sinhouse2@gmail.com>
2023-10-17 18:43:34 +0900

    ^Q^~

--- test.py
+++ test.py
@@ -1,52 +1,15 @@
-import aiohttp
-import asyncio
-import feedparser
 import json
-import os
 
-async def fetch_youtube_video(channel_id, count: int = 0) -> dict | None:
-    async with aiohttp.ClientSession() as session:
-        async with session.get(f'https://www.youtube.com/feeds/videos.xml?channel_id={channel_id}', allow_redirects=True) as response:
-            if response.status == 200:
-                video_ids = {}
-                data = await response.text()
-                feed = await asyncio.to_thread(feedparser.parse, data)
-                for i in range(len(feed["entries"])):
-                    video_id = feed["entries"][i]["yt_videoid"]
-                    published = feed["entries"][i]["published"]
-                    if feed["entries"][i]["updated"]:
-                        updated = feed["entries"][i]["updated"]
-                    else:
-                        updated = ""
-                    views = feed["entries"][i]["media_statistics"]["views"]
-                    video_ids[video_id] = {
-                        "published": published,
-                        "updated": updated,
-                        "views": views
-                    }
-                with open("test.json", "w")as f:
-                    json.dump(video_ids, f, indent=4)
-                return video_ids
-            elif count < 2:
-                return await fetch_youtube_video(channel_id, count + 1)
-            else:
-                return None
+with open("data/youtube_channels.json", "r")as f:
+    youtube_chhannels = json.load(f)
 
-async def check_youtube(channel_id):
-    video_ids = await fetch_youtube_video(channel_id)
-    if video_ids != None:
-        for video_id in video_ids:
-            youtube_channels[channel_id]["video_id"][video_id] = video_ids[video_id]
-        with open(f"{os.path.realpath(os.path.dirname(__file__))}/data/youtube_channels.json", "w") as f : 
-            json.dump(youtube_channels, f, indent=4)
+for channel_id in youtube_chhannels:
+    youtube_chhannels[channel_id] = {
+        "channel_name": youtube_chhannels[channel_id]["channel_name"],
+        "channel_image_url": youtube_chhannels[channel_id]["channel_image_url"],
+        "channel_description": youtube_chhannels[channel_id]["channel_description"],
+        "video_id": youtube_chhannels[channel_id]["video_id"]
+    }
 
-async def loop_check():
-    await asyncio.gather(*[check_youtube(channel_id) for channel_id in youtube_channels])
-
-async def before_loop_check():
-    global youtube_channels
-    with open(f"{os.path.realpath(os.path.dirname(__file__))}/data/youtube_channels.json", "r") as f:
-        youtube_channels = json.load(f)
-
-asyncio.run(before_loop_check())
-asyncio.run(loop_check())
+with open("data/youtube_channels.json", "w")as f:
+    json.dump(youtube_chhannels, f, indent=4)
